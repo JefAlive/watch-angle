92bc1fdde5adbafed536d939469481ca9fb23df0
jeferson <jef_hawk@hotmail.com>
2019-08-24 16:10:50 -0300

    response Content-Type: application/json

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// express
 var express = require('express');
 var app = express();
 var router = express.Router();
@@ -5,7 +6,13 @@ var router = express.Router();
 // routes
 var clockRoutes = require('./api/v0/clock/clock.routes.js');
 
+app.use(function (req, res, next) {
+    res.header('Content-Type', 'application/json');
+    next();
+});
+
 app.use('/v1/rest', router);
+
 clockRoutes(router);
 
 app.listen(3000, () => {
